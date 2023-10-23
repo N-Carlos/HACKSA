4f44e11612ff826bc749aaf6d4dbab7d2f98c5e0
Rix95 <ricalrax@gmail.com>
2023-10-23 17:42:41 -0500

    add button and fetch from api

--- hacksa/src/App.js
+++ hacksa/src/App.js
@@ -1,10 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [activity, setActivity] = useState("");
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://www.boredapi.com/api/activity/");
+      const data = await response.json();
+      setActivity(data.activity);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  fetchData();
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +30,13 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <h1>Feeling Bored? How about...</h1>
+          <button type="button" onClick={fetchData}>
+            {"Click me PLEASEEE"}
+          </button>
+          <p>{activity}</p>
+        </div>
       </header>
     </div>
   );
